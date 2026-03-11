82500cd4ffcd17ab64945ca7b39be872683a4c16
hafeez03 <hafeez_03@hotmail.com>
2026-03-11 15:00:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-// Form submission
-document.querySelector('form').addEventListener('submit', function(e){
-    e.preventDefault();
-    alert('Thank you! Your message has been sent.');
-});
+let slideIndex = 0;
 
-// Mobile menu toggle (optional)
-const nav = document.querySelector('header nav ul');
-document.querySelector('header h1').addEventListener('click', ()=>{
-    nav.classList.toggle('show');
-});
+showSlides();
+
+function showSlides(){
+
+let slides = document.getElementsByClassName("slide");
+
+for(let i=0;i<slides.length;i++){
+slides[i].style.display="none";
+}
+
+slideIndex++;
+
+if(slideIndex > slides.length){
+slideIndex = 1;
+}
+
+slides[slideIndex-1].style.display="block";
+
+setTimeout(showSlides,3000);
+
+}
